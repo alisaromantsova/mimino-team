fbbf1679b99901ab86c0f7900f5805136309b480
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2022-12-02 15:03:13 +0000

    Deploying to gh-pages from @ alisaromantsova/mimino-team@8fb2e17687588010cbea6c1ecef8eebea13ef88a 🚀

---
+++ room.39e6df98.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-room-open]"),closeModalBtn:document.querySelector("[data-modal-room-close]"),modal:document.querySelector("[data-modal-room]")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("modal-open")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=room.39e6df98.js.map
