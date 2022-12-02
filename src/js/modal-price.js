8ff250590b9fdc942d2e3c9d281de66d2ff3cf2c
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2022-12-02 23:27:48 +0200

    123

--- src/js/modal-price.js
+++ src/js/modal-price.js
@@ -13,3 +13,45 @@
     document.body.classList.toggle('modal-open');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-price-1-open]'),
+    modal: document.querySelector('[data-modal-price-1]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-price-2-open]'),
+    modal: document.querySelector('[data-modal-price-2]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-price-3-open]'),
+    modal: document.querySelector('[data-modal-price-3]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+})();
