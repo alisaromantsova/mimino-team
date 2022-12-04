86a9c6cb60a25d2eac306b5a8ddb84ca66e06544
alisaromantsova <alis.romantsova@gmail.com>
2022-12-04 02:23:55 +0200

    all

--- src/js/modal-table.js
+++ src/js/modal-table.js
@@ -1,32 +1,4 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
 
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//     document.body.classList.toggle('modal-open');
-//   }
-// })();
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open-1]'),
-//     modal: document.querySelector('[data-modal-1]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//     document.body.classList.toggle('modal-open');
-//   }
-// })();
 
 const refsTable = {
     openModalBtn: document.querySelector('[data-modal-open]'),
