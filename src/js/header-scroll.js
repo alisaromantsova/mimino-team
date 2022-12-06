bb59143ede652656b34434af62bc2848409fef98
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2022-12-06 21:49:24 +0200

    last-day-fix

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -2,4 +2,7 @@ window.onscroll = function () {
   scrollY <= 70
     ? document.getElementById('header').classList.remove('header-scroll')
     : document.getElementById('header').classList.add('header-scroll');
+  scrollY <= 250
+    ? document.getElementById('up-button').classList.remove('up-button-visible')
+    : document.getElementById('up-button').classList.add('up-button-visible');
 };
